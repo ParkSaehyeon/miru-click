747639f9e0c74e007860568172f189a7266b4797
Saehyeon <111236651+ParkSaehyeon@users.noreply.github.com>
2026-04-08 06:29:21 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
     plugins: [react()],
-    base: './',
+    base: '/miru-click/',
     server: {
         port: 3025,
         open: false,
